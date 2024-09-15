03e2e9fe9c2cea99a860f95563bbbf7f9726c244
shivPosted <shivpratap621@gmail.com>
2024-09-15 18:25:56 +0530

    building pages and linking them with router

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,3 +1,36 @@
+import { useState } from "react";
+import styles from "./Login.module.css";
 export default function Login() {
-  return <div>Login</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState(null);
+
+  return (
+    <main className={styles.login}>
+      <form className={styles.form}>
+        <div className={styles.row}>
+          <label htmlFor="email">Email address</label>
+          <input
+            type="email"
+            id="email"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          />
+        </div>
+
+        <div className={styles.row}>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+          />
+        </div>
+
+        <div>
+          <button>Login</button>
+        </div>
+      </form>
+    </main>
+  );
 }
--- src/pages/Pricing.jsx
+++ src/pages/Pricing.jsx
@@ -1,10 +1,28 @@
 import PageNav from "../components/PageNav";
+import styles from "./Product.module.css";
 const Pricing = () => {
   return (
-    <>
+    <main className={styles.product}>
       <PageNav />
-      <div>Pricing</div>
-    </>
+      <section>
+        <div>
+          <h2>
+            Simple pricing.
+            <br />
+            Just $9/month.
+          </h2>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae vel
+            labore mollitia iusto. Recusandae quos provident, laboriosam fugit
+            voluptatem iste.
+          </p>
+        </div>
+        <img
+          src="./worldwisePrice.png"
+          alt="overview of a large city with skyscrapers"
+        />
+      </section>
+    </main>
   );
 };
 
