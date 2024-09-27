b369a9ab4b52c7317003fcae0cebcdf4f71cd0e7
shivPosted <shivpratap621@gmail.com>
2024-09-27 19:43:52 +0530

    making city data fetching dynamic by getting id from url and making fake api request

--- src/components/CityDetailsCard.jsx
+++ src/components/CityDetailsCard.jsx
@@ -2,16 +2,24 @@ import { useNavigate, useParams, useSearchParams } from "react-router-dom";
 import styles from "./CityDetailsCard.module.css";
 import { dateFormatter } from "./util";
 import Button from "./Button";
+import { useEffect, useState } from "react";
 
 export default function CityDetailsCard() {
   // TEMP DATA
+  const { id } = useParams();
+
   const navigate = useNavigate();
-  const currentCity = {
-    cityName: "Jaipur",
-    countryCode: "in",
-    date: "2027-10-31T15:59:59.138Z",
-    notes: "My favorite city so far!",
-  };
+  const [currentCity, setCurrentCity] = useState({});
+
+  useEffect(() => {
+    fetch(`http://localhost:9000/cities/${id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        const { cityName, countryCode, date, notes } = data;
+        console.log(date);
+        setCurrentCity({ cityName, countryCode, date, notes });
+      });
+  }, [id]);
 
   const { cityName, countryCode, date, notes } = currentCity;
   return (
@@ -46,7 +54,7 @@ export default function CityDetailsCard() {
       </div>
       <div>
         <Button type="back" onclick={() => navigate(-1)}>
-          &larr;Back
+          &larr; Back
         </Button>
       </div>
     </div>
--- src/components/util.jsx
+++ src/components/util.jsx
@@ -1,8 +1,10 @@
 const dateFormatter = function (date) {
+  if (!date) return;
   return new Intl.DateTimeFormat("en", {
     weekday: "short",
     year: "numeric",
     month: "short",
+    day: "2-digit",
   }).format(new Date(date));
 };
 
