6d6b8b2810d658ccc7589c649dc4ea20545a79c2
shivPosted <shivpratap621@gmail.com>
2024-09-28 00:41:52 +0530

    adding active class in city detail section conditionally

--- src/components/CityCard.jsx
+++ src/components/CityCard.jsx
@@ -2,14 +2,15 @@ import { Link, useParams } from "react-router-dom";
 import styles from "./CityCard.module.css";
 import { dateFormatter } from "./util";
 import { useCitiesContext } from "./CitiesContext";
+
 export default function CityCard({ city }) {
   const { cityName, country, countryCode, date, id, position } = city;
   const { currentCity } = useCitiesContext();
   return (
-    <li className={currentCity.id === id ? styles["cityItem--active"] : ""}>
+    <li>
       <Link
         to={`${id}?lat=${position.lat}&lng=${position.lng}`}
-        className={`${styles.cityItem}`}
+        className={`${styles.cityItem} ${currentCity.id === id ? styles["cityItem--active"] : ""}`}
       >
         <img
           src={`https://flagcdn.com/24x18/${countryCode}.png`}
