cfbd32a2a97a3f9e540239f29a26e9b81fb59db8
shivPosted <shivpratap621@gmail.com>
2024-10-01 00:55:37 +0530

    designing the full fake AuthContext with fake user

--- src/components/AuthContext.jsx
+++ src/components/AuthContext.jsx
@@ -1,9 +1,60 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useReducer } from "react";
+
+const FAKE_USER = {
+  name: import.meta.env.VITE_FAKE_NAME,
+  email: import.meta.VITE_FAKE_EMAIL,
+  password: import.meta.env.VITE_FAKE_PASSWORD,
+  avatar: import.meta.env.VITE_FAKE_AVATAR,
+};
+
+const initialState = {
+  isAuthorized: false,
+  user: null,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "login":
+      return {
+        ...state,
+        isAuthorized: true,
+        user: action.payload,
+      };
+
+    case "logout":
+      return {
+        ...initialState,
+      };
+    default:
+      throw new Error("The action type for reducer in authconext not defined");
+  }
+}
 
 const AuthContext = createContext();
 
 function AuthProvider({ children }) {
-  return <AuthContext.Provider>{children}</AuthContext.Provider>;
+  const [{ isAuthorized, user }, dispatch] = useReducer(reducer, initialState);
+
+  function login(email, password) {
+    if (email === FAKE_USER.email && password === FAKE_USER.password)
+      dispatch({ type: "login", payload: FAKE_USER });
+  }
+
+  function logout() {
+    dispatch({ type: "logout" });
+  }
+  return (
+    <AuthContext.Provider
+      value={{
+        login,
+        logout,
+        user,
+        isAuthorized,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
 }
 
 function useAuth() {
